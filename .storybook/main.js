e4428888f91ba1f3770410ba080c705a80e6f09f
Fellpe Pinheiro <fellipe@taller.net.br>
2020-06-12 09:58:26 -0300

    clean

--- .storybook/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  stories: ['../src/**/*.stories.js', '../src/**/story.tsx'],
-  addons: [
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-  ],
-};
--- src/components/Box/index.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-import { color, space } from 'styled-system'
-
-const Box = styled.div`
-  ${color}
-  ${space}
-`
-
-export default Box;
