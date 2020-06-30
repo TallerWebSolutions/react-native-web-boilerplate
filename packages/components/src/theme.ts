29481ff14322a5ad6fd49b8307fb96a8a9a50831
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2020-06-30 18:31:35 -0300

    feat: add theme colors and cardVariant

--- packages/components/src/theme.ts
+++ packages/components/src/theme.ts
@@ -1,9 +1,3 @@
-import {
-  BackgroundColorProps,
-  backgroundColor,
-  BaseTheme,
-} from '@shopify/restyle';
-
 const palette = {
   purpleLight: '#8C6FF7',
   purplePrimary: '#5A31F4',
@@ -16,30 +10,33 @@ const palette = {
   black: '#0B0B0B',
   white: '#fff',
   red: '#ff0000',
-
-  // primary: {
-  //   deep: '#210D26',
-  //   dark: '#3A2041',
-  //   main: '#8B7696',
-  //   light: '#D2CEDE',
-  //   clear: '#E7E4F1',
-  // },
-
-  // neutral: {
-  //   deep: '#292929',
-  //   dark: '#505050',
-  //   main: '#747474',
-  //   light: '#A9A9A9',
-  //   clear: '#EEEEEE',
-  // },
 };
+
+const primary = {
+  deep: '#210D26',
+  dark: '#3A2041',
+  main: '#8B7696',
+  light: '#D2CEDE',
+  clear: '#E7E4F1',
+};
+
+// neutral: {
+//   deep: '#292929',
+//   dark: '#505050',
+//   main: '#747474',
+//   light: '#A9A9A9',
+//   clear: '#EEEEEE',
+// },
 
 const theme = {
   colors: {
     ...palette,
     mainBackground: palette.white,
     cardPrimaryBackground: palette.purplePrimary,
-    // white: 'white',
+    primaryDeep: primary.deep,
+    primaryDark: primary.dark,
+    primaryMain: primary.main,
+    primaryLight: primary.light,
   },
   spacing: {
     s: 8,
@@ -53,7 +50,13 @@ const theme = {
   },
   cardVariants: {
     primary: {
-      backgroundColor: 'white',
+      backgroundColor: 'mainBackground',
+      shadowOffset: { width: 0, height: 0 },
+      shadowColor: 'primaryLight',
+      shadowOpacity: 0.8,
+      shadowRadius: 6,
+      elevation: 5,
+      borderRadius: 10,
     },
     secondary: {
       // backgroundColor: 'primaryDeep,
